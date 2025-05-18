0414cbaf0b880216beb24a5f0542e0b6eeba7022
zj <2237696890@qq.com>
2025-05-18 17:10:28 +0800

    Complete the administrator login API and set the initial interface as the login interface

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import UserListView from '../views/UserListView.vue'
-// 暂时注释掉未实现的组件路由
 import AnnouncementForm from '../views/announcement/AnnouncementForm.vue'
 import ArticleEditor from '../views/article/ArticleEditor.vue'
 
 const routes = [
   { path: '/login', component: LoginView },
-  { path: '/', redirect: '/users' },
+  { path: '/', redirect: '/login' },
   { path: '/users', component: UserListView },
-  // 暂时注释掉未实现的路由
   { path: '/announcements', component: AnnouncementForm },
   { path: '/articles', component: ArticleEditor }
 ]
