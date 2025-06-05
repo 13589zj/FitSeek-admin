f7daf3651d65c863b0012d01c5c97fd93631a779
Fanovian <fanovian@outlook.com>
2025-06-05 15:29:47 +0800

    gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import UserListView from '../views/UserListView.vue'
 import AnnouncementForm from '../views/announcement/AnnouncementForm.vue'
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
